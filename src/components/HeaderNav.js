5564e5979160c1e41d21b337185b8f5492f37dc9
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-11-02 20:09:38 +0100

    Header and Hero mobile view done

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -6,10 +6,10 @@ const HeaderNav = () => {
   return(
     <nav className="header-nav">
       <ul className="header-nav__list">
-        <li><Link to="about" smooth={true} duration={750}>About</Link></li>
-        <li><Link to="skills" smooth={true} duration={750}>Skills</Link></li>
-        <li><Link to="projects" smooth={true} duration={750}>Projects</Link></li>
-        <li><Link to="contact" smooth={true} duration={750}>Contact</Link></li>
+        <li className="nav__list-item nav-about"><Link to="about" smooth={true} duration={750}>About</Link></li>
+        <li className="nav__list-item"><Link to="skills" smooth={true} duration={750}>Skills</Link></li>
+        <li className="nav__list-item"><Link to="projects" smooth={true} duration={750}>Projects</Link></li>
+        <li className="nav__list-item nav-contact"><Link to="contact" smooth={true} duration={750}>Contact</Link></li>
       </ul>
     </nav>
   );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import {Link} from 'react-scroll';
 import '../styles/Hero.scss';
-import down from '../images/logo-down.png';
+import down from '../images/claro.png';
 
 const Hero = () => {
   return (
     <div className="header-hero_wrapper">
-      <h1 className="hero_name appear-effect">Alexandra Jara</h1>
-      <span className="appear-front">
-        <p className="hero_frontend">Front-end developer</p>
-      </span>
-      <Link to="about" smooth={true} duration={500} className="hero__link-down"><img src={down} alt="Go down arrow" className="down-img"/></Link>
+      <div className="hero__text-wrapper">
+        <h1 className="hero_name appear-effect">Alexandra Jara</h1>
+        <span className="appear-front">
+          <p className="hero_frontend">Front-end developer</p>
+        </span>
+      </div>
+      <Link to="about" smooth={true} duration={400} className="hero__link-down"><img src={down} alt="Go down arrow" className="down-img"/></Link>
     </div>
   );
 }
