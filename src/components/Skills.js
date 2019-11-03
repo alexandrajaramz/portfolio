490a8ae1f6ba3a01481f4cf051d08cd0ce47ff16
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-11-03 19:51:25 +0100

    Skills mobile view done

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import '../styles/Skills.scss';
 
 const Skills = () => {
   return(
     <section className="main-skills" id="skills">
+      <h2 className="skills__title">Skills</h2>
       <ul className="skills__hardskills">
-        <li className="hardskills-item">HTML5</li>
-        <li className="hardskills-item">CSS3</li>
-        <li className="hardskills-item">JAVASCRIPT ES6</li>
-        <li className="hardskills-item">REACT JS</li>
-        <li className="hardskills-item">SASS</li>
-        <li className="hardskills-item">FLEXBOX & GRID</li>
-        <li className="hardskills-item">NPM</li>
-        <li className="hardskills-item">GIT</li>
+        <li className="hardskills-item html">HTML5</li>
+        <li className="hardskills-item css ">CSS3</li>
+        <li className="hardskills-item sass">SASS</li>
+        <li className="hardskills-item flexbox">FLEXBOX</li>
+        <li className="hardskills-item grid">GRID</li>
+        <li className="hardskills-item javascript">JAVASCRIPT</li>
+        <li className="hardskills-item react">REACT</li>
+        <li className="hardskills-item npm">NPM</li>
+        <li className="hardskills-item git">GIT</li>
+        <li className="hardskills-item agile">AGILE</li>
+
       </ul>
     </section>
   );
