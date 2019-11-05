23d9323243bd6b1f99ea7f0dc037e492912a982c
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-11-05 21:48:30 +0100

    Build docs

---
+++ docs/precache-manifest.11ea814a6fe57935df6ac5d252b2df03.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cab54e1512bdaf8aabaf557351910ab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a2f4d2021def606b513e",
+    "url": "./static/css/main.7e6fed5f.chunk.css"
+  },
+  {
+    "revision": "87efb67d17cef5da01b3",
+    "url": "./static/js/2.18cd1832.chunk.js"
+  },
+  {
+    "revision": "a2f4d2021def606b513e",
+    "url": "./static/js/main.89cd0617.chunk.js"
+  },
+  {
+    "revision": "dda1c863fd1c52192d0c",
+    "url": "./static/js/runtime-main.c5db20d9.js"
+  },
+  {
+    "revision": "2821f5fc2cc7c6b29d96615cba7d750e",
+    "url": "./static/media/alex-1.2821f5fc.png"
+  },
+  {
+    "revision": "b64b19d54989dc25488fd1634743a351",
+    "url": "./static/media/anonymous-proxy.b64b19d5.png"
+  },
+  {
+    "revision": "749a76dfba62278db2625d7bc1cdff6e",
+    "url": "./static/media/bg.749a76df.jpg"
+  },
+  {
+    "revision": "9eec28d8e81923ce1a3a4c2d97b42085",
+    "url": "./static/media/down.9eec28d8.png"
+  },
+  {
+    "revision": "93f36507a8e72b7c62a54cf781928051",
+    "url": "./static/media/fivedrops.93f36507.png"
+  },
+  {
+    "revision": "8d65d7436c43600df7949238b7a1a196",
+    "url": "./static/media/profile-cards.8d65d743.png"
+  },
+  {
+    "revision": "1f4c2d8a1b8129bd4be2f88d333d1bc9",
+    "url": "./static/media/refactoring-react.1f4c2d8a.png"
+  },
+  {
+    "revision": "4d13233f8b37d342b36bdcb9e58bcdd8",
+    "url": "./static/media/rick-morty.4d13233f.png"
+  },
+  {
+    "revision": "bf523edbdff1e96f30878a00260bb2f1",
+    "url": "./static/media/series-finder.bf523edb.png"
+  },
+  {
+    "revision": "f30025fe940456ceeac8b07a33d67414",
+    "url": "./static/media/travel-wallet.f30025fe.png"
+  }
+]);
